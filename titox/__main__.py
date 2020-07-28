910c0dc31b9157746009c983e3c01da3fe00a697
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-07-28 22:40:38 +0200

    stub arguments

--- titox/__main__.py
+++ titox/__main__.py
@@ -1,4 +1,13 @@
+from argparse import ArgumentParser, FileType
+
+
 def main():
-    print('running module titox')
+
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--items', nargs='+', required=True)
+    parser.add_argument('-j', '--j2', type=FileType('r'), required=True)
+    args = parser.parse_args()
+
+
 if __name__ == '__main__':
     main()
